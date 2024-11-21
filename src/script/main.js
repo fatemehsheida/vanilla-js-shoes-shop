442d9b6de8f6c8d9600381d63b8b9a84eb3a2c87
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2024-11-21 22:17:31 +0330

    Front page photo

--- src/script/main.js
+++ src/script/main.js
@@ -1,19 +1,6 @@
 import { ce } from "../Utils/create-element.js";
-import layout from "../layout/layout.js";
-
-const layout1 = layout();
-console.log(layout1);
-document.getElementById("app").appendChild(layout());
-document.getElementById("app").appendChild(
-  ce("div", {
-    className:
-      "w-[150px] h-[150px] bg-slate-500 text-white font-bold text-2xl text-center pt-10 mt-4 mx-auto rounded-full hover:ring-8 ring-pink-500 ring-offset-4 cursor-pointer ",
-    innerText: "JUST FOR TEST",
-    events: {
-      click: function () {
-        let msg = prompt("test is passed").toString();
-        alert(msg);
-      },
-    },
-  })
-);
+import layout from "../layout/welcome/layout.js";
+import fe from "../Utils/findElements.js";
+let welcome = fe("welcome");
+welcome.className = "w-full h-dvh";
+welcome.appendChild(layout());
