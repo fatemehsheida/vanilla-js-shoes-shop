8e954d46537ccf55aa9a986f2b1f877c2dcda5f1
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2024-11-21 21:57:54 +0330

    El function test

--- src/script/main.js
+++ src/script/main.js
@@ -4,3 +4,16 @@ import layout from "../layout/layout.js";
 const layout1 = layout();
 console.log(layout1);
 document.getElementById("app").appendChild(layout());
+document.getElementById("app").appendChild(
+  ce("div", {
+    className:
+      "w-[150px] h-[150px] bg-slate-500 text-white font-bold text-2xl text-center pt-10 mt-4 mx-auto rounded-full hover:ring-8 ring-pink-500 ring-offset-4 cursor-pointer ",
+    innerText: "JUST FOR TEST",
+    events: {
+      click: function () {
+        let msg = prompt("test is passed").toString();
+        alert(msg);
+      },
+    },
+  })
+);
