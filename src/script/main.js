ae8c4136f19eaae334113a0cf66d2a325e440938
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2024-11-21 22:55:47 +0330

    add image page2

--- src/script/main.js
+++ src/script/main.js
@@ -1,8 +1,10 @@
 import { ce } from "../Utils/create-element.js";
 import fe from "../Utils/findElements.js";
 import layout from "../layout/welcome/layout.js";
-import welcome1 from "../pages/onboarding-page-1.js";
+import wel1 from "../pages/onboarding-page-1.js";
+import wel2 from "../pages/onboarding-page-2.js";
+import login from "../pages/login.js";
 
 let welcome = fe("welcome");
 welcome.className = "w-full h-dvh";
-welcome.appendChild(welcome1());
+welcome.appendChild(login());
