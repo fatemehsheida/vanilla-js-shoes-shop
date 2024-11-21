8d6836182cdec946f96d7b4f1183f3178ca461ac
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2024-11-21 22:19:59 +0330

    Complete the welcome page

--- src/layout/welcome/layout.js
+++ src/layout/welcome/layout.js
@@ -4,8 +4,9 @@ import spiner from "../../components/loading.js";
 export default function layout() {
   const layout = ce("div", {
     children: [logo(), spiner()],
+    restAttrs: { id: "loading" },
     className:
-      " container h-full w-full flex flex-col justify-center items-center",
+      " loading h-full w-full flex flex-col justify-center items-center",
   });
   return layout;
 }
