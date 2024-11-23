77bf2480af0c703a69c2e14bbdb1d978e2f1474d
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2024-11-23 20:00:49 +0330

    create card Detail

--- src/pages/fetchCardDetail.js
+++ src/pages/fetchCardDetail.js
@@ -3,21 +3,187 @@ import clog from "../Utils/logdata.js";
 import { ce } from "../Utils/create-element.js";
 import getAdiddasProductsList from "../api/addidas.api.js";
 import cardElement from "../components/card.js";
-import {router} from "../routes/router.js";
-function createSize(product){
-  let sizeContainer=ce("div",{
-    className:"w-full flex flex-row justify-center items-center gap-2"
+export default function fetchCardDetail(data = {}) {
+  let test2 = ce("div", {
+    className: "w-ful h-screen ",
   });
-  product.size.forEach(sizeNum=>{
-     let sizeElem=ce("div",{
-      className:"rounded-full w-5 h-5 p-3 border-solid border-2 border-slate-800 flex justify-center items-center",
-      children:[ce("p",{
-        innerText:sizeNum,
-        className:"text-xs font-bold"
-      })]
-    });
-     sizeContainer.appendChild(sizeElem);
-  })
-  return sizeContainer;
-}
+  fetchCardByImage(data).then((products) => {
+    products.forEach((product) => {
+      clog(product);
 
+      let test3 = ce("div", {
+        className: "detailContainer w-full h-screen flex flex-col ",
+        children: [
+          ce("div", {
+            className: "w-full h-1/3",
+            children: [
+              ce("i", {
+                className: "fa-solid fa-backward cursor-pointer ",
+                events: {
+                  click: () => {
+                    router.navigate("/home");
+                  },
+                },
+              }),
+            ],
+          }),
+          ce("div", {
+            className: "infoContainer w-full h-2/3 px-6",
+            children: [
+              ce("div", {
+                className: "width-full h-1/4  py-7",
+                children: [
+                  ce("h1", {
+                    innerText: product.title,
+                    className: "font-bold text-4xl w-full h-2/3 ",
+                  }),
+                  ce("i", {
+                    className: "popularHeart",
+                  }),
+                  ce("div", {
+                    className:
+                      "w-full h-1/3  flex flex-row justify-start items-center",
+                    children: [
+                      ce("div", {
+                        className:
+                          "w-1/5 h-full bg-slate-200 rounded-lg flex justify-center items-center px-2 py-1",
+                        children: [
+                          ce("p", {
+                            className: "font-bold text-[11px] text-slate-700",
+                            innerText: "5 ,371 solid",
+                          }),
+                          ce("div", {
+                            className: "",
+                            children: [
+                              ce("i", {
+                                className: "starNumber",
+                              }),
+                              ce("p", {
+                                className:
+                                  "font-semibold text-[11px] text-slate-700",
+                                innerText: "4.3(5.389 reviews)",
+                              }),
+                            ],
+                          }),
+                        ],
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+            ],
+          }),
+          ce("div", {
+            className: "detailMiddlePart",
+            children: [
+              ce("div", {
+                className: "",
+                children: [
+                  ce("h1", {
+                    className: "",
+                    innerText: "Description",
+                  }),
+                  ce("p", {
+                    className: "",
+                    innerText:
+                      "lorem ipsum dolor sit amet, consectatur adipiscing elit,sad do eiusmod tempor incididunt ut labore et ",
+                  }),
+                ],
+              }),
+              ce("div", {
+                className: "selectSection",
+                children: [
+                  ce("div", {
+                    className: "sizingSection",
+
+                    children: [
+                      ce("h3", {
+                        className: "",
+                        innerText: "Size",
+                      }),
+                      ce("", {
+                        className: "",
+                        innerText,
+                      }),
+                    ],
+                  }),
+                  ce("div", {
+                    className: "colorsSection",
+
+                    children: [
+                      ce("h3", {
+                        className: "",
+                        innerText: "Color",
+                      }),
+                      ce("", {
+                        className: "",
+                        innerText,
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+              ce("div", {
+                className: "numberSection",
+
+                children: [
+                  ce("h3", {
+                    className: "",
+                    innerText: "Quantity",
+                  }),
+                  ce("div", {
+                    className: "",
+                    children: [
+                      ce("i", {
+                        className: "",
+                      }),
+                      ce("div", {
+                        className: "",
+                      }),
+                      ce("i", {
+                        className: "",
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+            ],
+          }),
+          ce("div", {
+            className: "payment",
+            children: [
+              ce("div", {
+                className: "",
+                children: [
+                  ce("h5", {
+                    className: "",
+                    innerText: "Total price",
+                  }),
+                  ce("h5", {
+                    className: "",
+                    innerText: `$${257}.00`,
+                  }),
+                ],
+              }),
+              ce("div", {
+                className: "",
+                children: [
+                  ce("i", {
+                    className: "",
+                  }),
+                  ce("p", {
+                    className: "",
+                    innerText: "Add to Cart",
+                  }),
+                ],
+              }),
+            ],
+          }),
+        ],
+      });
+      test2.append(test3);
+    });
+    return test2;
+  });
+  return test2;
+}
--- src/script/main.js
+++ src/script/main.js
@@ -1,49 +1,23 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import "./src/index.css";
-import { App } from "../App.js";
-import { router } from "./../routes/router.js";
 
+import {ce} from "../Utils/create-element.js";
+import fe from "../Utils/findElements.js";
+import loadingPage from "../pages/onboarding/LoadingPage.js";
+import onboardingPage2 from "../pages/onboarding/onboarding-page-2.js";
+import onboardingPage3 from "../pages/onboarding/onboarding-page-3.js";
+import onboardingPage4 from "../pages/onboarding/onboarding-page-4.js";
+import onboardingPage5 from "../pages/onboarding/onboarding-page-5.js";
+import login from "../pages/login.js";
+import fetchAllProducts from "../pages/fetchAllProducts.js";
+import getProduct from "../api/products.api.js";
+import fetchAllLogo from "../pages/fetchLogo.js";
+import searchInput from "../components/searchInput.js";
+import most from "../pages/homeMostNav.js";
+import footer from "../pages/footer.js";
+import home from "../pages/home.js";
+import fetchAdiddasProducts from "../pages/fetchAdiddasProducts.js";
+import {App} from "../App.js";
+import { router } from "./../routes/router.js";
 const root = fe("app");
-root.className = "w-full h-screen";
+root.className="w-full h-screen";
 root.appendChild(App());
 router.resolve();
-=======
-=======
->>>>>>> feature/onboarding
-import { ce } from "../Utils/create-element.js";
-import fe from "../Utils/findElements.js";
-import layout from "../layout/welcome/layout.js";
-import wel1 from "../pages/onboarding-page-1.js";
-import wel2 from "../pages/onboarding-page-2.js";
-<<<<<<< HEAD
-import onboardingPage3 from "../pages/onboarding-page-3.js";
-import onboardingPage4 from "../pages/onboarding-page-4.js";
-import onboardingPage5 from "../pages/onboarding-page-5.js";
-import loadingPage from "../pages/LoadingPage.js";
-import onboardingPage2 from "../pages/onboarding-page-2.js";
-
-import login from "../pages/login.js";
-let welcome = fe("welcome");
-welcome.className = "w-full h-dvh";
-welcome.appendChild(login());
->>>>>>> feature/login
-=======
-import login from "../pages/login.js";
-import onboardingPage3 from "../pages/onboarding-page-3.js";
-import onboardingPage4 from "../pages/onboarding-page-4.js";
-import onboardingPage5 from "../pages/onboarding-page-5.js";
-
-let welcome = fe("welcome");
-welcome.className = "w-full h-dvh";
-welcome.appendChild(onboardingPage5());
->>>>>>> feature/onboarding
-=======
-import { ce } from "../Utils/create-element.js";
-import layout from "../layout/welcome/layout.js";
-import fe from "../Utils/findElements.js";
-let welcome = fe("welcome");
-welcome.className = "w-full h-dvh";
-welcome.appendChild(layout());
->>>>>>> welcome
