81cc1de7fe716085a06b171af4c4345831609ed8
Fshd5232 <f.sh.d5232@gmail.com>
2024-11-26 12:03:21 +0330

    complete the checkout header compenent and import it

--- src/pages/checkout/checkout-header.js
+++ src/pages/checkout/checkout-header.js
@@ -3,42 +3,45 @@ export default function header(text, more = false) {
   if (more == true) {
     let header = ce("div", {
       className:
-        "flex flex-row justify-between items-center bg-orange-300 w-full h-2/12",
+        "flex flex-row justify-between items-center py-3 w-full h-2/12",
       children: [
         ce("div", {
-          className:
-            "title flex flex-row justify-center items-center gap-3 bg-green-600",
+          className: "title flex flex-row justify-center items-center gap-3",
           children: [
             ce("i", {
               className: "fa-solid fa-backward",
             }),
             ce("h1", {
               innerText: text,
+              className: "text-2xl font-semibold",
             }),
           ],
         }),
         ce("i", {
-          className: "fa-solid fa-ellipsis",
+          className: "fa-solid fa-ellipsis text-2xl",
+        }),
+      ],
+    });
+  } else {
+    header = ce("div", {
+      className:
+        "flex flex-row justify-between items-center bg-orange-300 w-full h-12",
+      children: [
+        ce("div", {
+          className:
+            "title flex flex-row justify-center items-center gap-4 bg-green-600",
+          children: [
+            ce("i", {
+              className: "fa-solid fa-backward ",
+            }),
+            ce("h1", {
+              className: "text-2xl font-semibold",
+              innerText: text,
+            }),
+          ],
         }),
       ],
     });
   }
-  let header = ce("div", {
-    className: "flex flex-row justify-between items-center bg-orange-300",
-    children: [
-      ce("div", {
-        className:
-          "title flex flex-row justify-center items-center gap-3 bg-green-600",
-        children: [
-          ce("i", {
-            className: "fa-solid fa-back",
-          }),
-          ce("h1", {
-            innerText: text,
-          }),
-        ],
-      }),
-    ],
-  });
   return header;
 }
--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -3,7 +3,7 @@ import { ce } from "../../Utils/create-element";
 import clog from "../../Utils/logdata";
 export default function checkout() {
   let checkout2 = ce("div", {
-    className: "w-full h-full bg-pink-600 flex flex-col",
+    className: "w-full h-screen bg-pink-600 flex flex-col px-6",
     children: [header("checkout", true)],
   });
   return checkout2;
